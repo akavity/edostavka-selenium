bcafc65783f63c445c8e27da001c44b5f1850cdb
Basil Akavity <vakavity@gmail.com>
2025-05-14 20:21:21 +0300

    allure, DriverManager, testNg have been added

---
+++ src/main/java/org/akavity/driver/DriverManager.java
@@ -0,0 +1,33 @@
+package org.akavity.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DriverManager {
+    private static WebDriver driver;
+
+    private DriverManager() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        try {
+            driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
